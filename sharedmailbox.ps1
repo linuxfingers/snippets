91c3da0e706a5d8673b428f258cfa3e53ce76a8c
linuxfingers <linuxfingers@protonmail.com>
2023-02-20 15:57:21 -0500

    Update sharedmailbox.ps1
    add other snip

--- sharedmailbox.ps1
+++ sharedmailbox.ps1
@@ -3,3 +3,9 @@
 Import-Module ExchangeOnlineManagement
 Connect-ExchangeOnline
 Get-Mailbox -RecipientTypeDetails SharedMailbox -ResultSize Unlimited | select PrimarySmtpAddress,DisplayName | Export-CSV ./sharedmb.csv
+
+
+# perms
+
+Connect-ExchangeOnline  
+Get-Mailbox -RecipientTypeDetails SharedMailbox -ResultSize:Unlimited | Get-MailboxPermission |Select-Object Identity,User,AccessRights | Where-Object {($_.user -like '*@*')}|Export-Csv ./csv/sharedmailbox.csv  -NoTypeInformation 
